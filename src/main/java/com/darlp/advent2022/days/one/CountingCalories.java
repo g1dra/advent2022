a48c01b453f32080db5d62ee49db06e62b4c91fb
g1dra <darko.vucetic7@gmail.com>
2022-12-02 01:18:29 +0100

    day one complete

--- src/main/java/com/darlp/advent2022/days/one/CountingCalories.java
+++ src/main/java/com/darlp/advent2022/days/one/CountingCalories.java
@@ -1,33 +1,27 @@
 package com.darlp.advent2022.days.one;
 
-import jakarta.annotation.Resource;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.Objects;
+import java.util.*;
 
 public class CountingCalories {
-    public static Integer findMaxCalories(String caloriesList) {
+    public static List<Integer> findMaxCalories(String caloriesList) {
         var inventory = caloriesList.split("\\R");
+        List<String> inventoryList = new ArrayList<>(List.of(inventory));
+        var ele = "";
+        inventoryList.add(ele);
         var currentCalories = 0;
-        var maxCalories = 0;
+        List<Integer> caloriesListByElf = new ArrayList<>();
 
-        for (String item : inventory) {
+        for (String item : inventoryList) {
             if (Objects.equals(item, "")) {
-                maxCalories = Math.max(currentCalories, maxCalories);
+                caloriesListByElf.add(currentCalories);
                 currentCalories = 0;
                 continue;
             }
             currentCalories += Integer.parseInt(item);
         }
 
-        System.out.println(maxCalories);
-        return maxCalories;
-    }
+        Collections.sort(caloriesListByElf);
 
-    public static Integer exercise1() throws IOException {
-        String caloriesList = Files.readString(Path.of("src/main/resources/input/day1.txt"));
-        return findMaxCalories(caloriesList);
+        return caloriesListByElf;
     }
 }
--- src/test/java/com/darlp/advent2022/days/one/CountingCaloriesTest.java
+++ src/test/java/com/darlp/advent2022/days/one/CountingCaloriesTest.java
@@ -3,36 +3,47 @@ package com.darlp.advent2022.days.one;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Objects;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CountingCaloriesTest {
+    String caloriesList = """
+            1000
+            2000
+            3000
+
+            4000
+
+            5000
+            6000
+
+            7000
+            8000
+            9000
+
+            10000""";
+
     @Test
-    public void day1demo() {
-        var caloriesList = """
-                1000
-                2000
-                3000
-
-                4000
-
-                5000
-                6000
-
-                7000
-                8000
-                9000
-
-                10000""";
-
+    public void exercise1Sample() {
         var maxCalories = CountingCalories.findMaxCalories(caloriesList);
-        assertEquals(24000, maxCalories);
+        assertEquals(24000, maxCalories.get(maxCalories.size() - 1));
     }
 
     @Test
-    void exercise1() throws IOException {
-        assertEquals(66306, CountingCalories.exercise1() );
+    public void exercise2Sample() {
+        var maxCalories = CountingCalories.findMaxCalories(caloriesList);
+        var subList = maxCalories.subList(maxCalories.size() - 3, maxCalories.size());
+        assertEquals(45000, subList.stream().reduce(0, Integer::sum));
+    }
+
+    @Test
+    public void day1Solution() throws IOException {
+        String caloriesListFile = Files.readString(Path.of("src/main/resources/input/day1.txt"));
+        var maxCalories = CountingCalories.findMaxCalories(caloriesListFile);
+        assertEquals(66306, maxCalories.get(maxCalories.size() - 1));
+        var top3 = maxCalories.subList(maxCalories.size() - 3, maxCalories.size()).stream().reduce(0, Integer::sum);
+        assertEquals(195292, top3);
     }
 }
